e697ac52ad80f3cb97765b7da3f49227c5203b7b
Win <synwe@yahoo.com>
2017-05-11 10:59:17 +0000

    Remove url patterns

--- opinion/urls.py
+++ opinion/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from opinion.views import PollDetailView, PollListView, PollVoteView, PollResultsView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', PollListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/results/$', PollResultsView.as_view(), name='results'),
     url(r'^(?P<pk>\d+)/vote/$', login_required(PollVoteView.as_view()), name='vote'),
     url(r'^(?P<pk>\d+)/$', PollDetailView.as_view(), name='detail'),
-)
+]
